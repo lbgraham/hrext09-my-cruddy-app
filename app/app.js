a745cfa2403db833da2ffbebc23bf008d0bf7738
Lee Graham <lbgraham@mac.com>
2019-05-17 18:19:46 -0700

    Updated CSS, updated the js to generate a random sentence using the data from the Twiddler program. Put the Twiddler data in a separate file

--- app/app.js
+++ app/app.js
@@ -1,74 +1,35 @@
-/*
-
- ### Basic Reqs
-- [ ] Where to store data? (localstorage)
-- [ ] How to caputure data? (web form)
-- [ ] How to modify data? (update action, delete action)
-- [ ] How to view data? (style?)
-- [ ] UI/UX considerations (how are we going to use this)
-
-*/
-
-//localStorage interaction function
-//get item
-var getItem = function(key) {
-  return window.localStorage.getItem(key);
-}
-
-//create
-var createItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
-}
-
-//update
-var updateItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
-}
-
-//delete
-var deleteItem = function(key) {
-  return window.localStorage.removeItem(key);
-}
-
-//clear everything
-var clearEverything = function() {
-  return window.localStorage.clear();
-}
-
-var keyExists = function(key) {
-  var currentValue = getItem(key);
-  return currentValue !== null;
-}
-
-
-///////////////////////////////////////////
-//event handlers for the buttons and ... possibly the inputboxes
-  //preventdefault on button clicks
 $(document).ready(function() {
-  
-  $( "#draggable" ).draggable();
+  window.streams = {};
+  streams.home = [];
+  streams.users = {};
+  window.users = Object.keys(streams.users);
 
-  $('#createButton').click(function(event) {
+  var randomElement = function(array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+
+  var randomMessage = function() {
+    return [
+      randomElement(opening),
+      randomElement(verbs),
+      randomElement(objects),
+      randomElement(nouns),
+      randomElement(tags)
+    ].join(" ");
+  };
+
+  var generateRandomTweet = function() {
+    var tweet = {};
+    tweet.message = randomMessage();
+  };
+
+  $('#generate-poem').click(function(event) {
     event.preventDefault();
-
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      //current key exists, do something error-handle-y
-    } else {
-      createItem(currentKey, currentValue);
-    }
+    var poem = "<p>" + randomMessage() + "</p>";
+    $( "#poem" ).replaceWith( poem );
   });
 
-  $('#updateButton').click(function(event) {
-    event.preventDefault();
+  $( "#magnets" ).draggable();
 
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      updateItem(currentKey, currentValue);
-    } else {
-      //current key doesnt exist, do stuff
-    }
-  });
 });
---
+++ app/data.js
@@ -0,0 +1,89 @@
+var opening = [
+  "just",
+  "",
+  "",
+  "",
+  "",
+  "ask me how i",
+  "completely",
+  "nearly",
+  "productively",
+  "efficiently",
+  "last night i",
+  "the president",
+  "that wizard",
+  "a ninja",
+  "a seedy old man"
+];
+
+var verbs = [
+  "downloaded",
+  "interfaced",
+  "deployed",
+  "developed",
+  "built",
+  "invented",
+  "experienced",
+  "navigated",
+  "aided",
+  "enjoyed",
+  "engineered",
+  "installed",
+  "debugged",
+  "delegated",
+  "automated",
+  "formulated",
+  "systematized",
+  "overhauled",
+  "computed"
+];
+
+var objects = [
+  "my",
+  "your",
+  "the",
+  "a",
+  "my",
+  "an entire",
+  "this",
+  "that",
+  "the",
+  "the big",
+  "a new form of"
+];
+
+var nouns = [
+  "cat",
+  "koolaid",
+  "system",
+  "city",
+  "worm",
+  "cloud",
+  "potato",
+  "money",
+  "way of life",
+  "belief system",
+  "security system",
+  "bad decision",
+  "future",
+  "life",
+  "pony",
+  "mind"
+];
+
+var tags = [
+  "#techlife",
+  "#burningman",
+  "#sf",
+  "but only i know how",
+  "for real",
+  "#sxsw",
+  "#ballin",
+  "#omg",
+  "#yolo",
+  "#magic",
+  "",
+  "",
+  "",
+  ""
+];
