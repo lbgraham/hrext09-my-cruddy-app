39b4dda57c143420598ea9c6f367a325ed924f68
Lee Graham <lbgraham@mac.com>
2019-05-17 21:16:16 -0700

    Added boxes around words/phrases

--- app/app.js
+++ app/app.js
@@ -18,13 +18,16 @@ $(document).ready(function() {
   $('#generate-poem').click(function(event) {
     event.preventDefault();
 
+    var boxStyle = 'style="display: flex; justify-content: center; \
+      align-text: center; height: 25px; border: 2px solid gray; padding: 20px"';
+
     $( 'p' ).remove();
 
     var poem = randomMessage();
 
     for(var i = 0; i < poem.length; i++) {
       if(poem[i] !== '') {
-        $( '#magnets' ).append( '<p id="poem-word' + i + '" style="padding-left: 30px">' + poem[i] + '</p>' );
+        $( '#magnets' ).append( '<p id="poem-word' + i + '" ' + boxStyle + '>' + poem[i] + '</p>' );
         $( '#poem-word' + i ).draggable();
       }
     }
