03a02f0709ffd6a0c337c06dc0084e316de4e1ad
Lee Graham <lbgraham@mac.com>
2019-05-19 20:42:47 -0700

    Minor formatting

--- app/plib.js
+++ app/plib.js
@@ -1,44 +1,44 @@
-  // Selects the random word for each part of speech
-  const randomElement = function(array) {
-    const randomIndex = Math.floor(Math.random() * array.length);
+// Selects the random word for each part of speech
+const randomElement = function(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
 
-    return array[randomIndex];
-  };
+  return array[randomIndex];
+};
 
-  const randomWords = function() {
-    let extraMagnetWords = [];
-    for(let i = 0; i < 20; i++) {
-      extraMagnetWords.push(words[Math.floor(Math.random() * words.length)]);
-    }
+const randomWords = function() {
+  let extraMagnetWords = [];
+  for(let i = 0; i < 20; i++) {
+    extraMagnetWords.push(words[Math.floor(Math.random() * words.length)]);
+  }
 
-    for(let i = 0; i < 3; i++) {
-       extraMagnetWords.push(tags[Math.floor(Math.random() * tags.length)]);
-    }
+  for(let i = 0; i < 3; i++) {
+     extraMagnetWords.push(tags[Math.floor(Math.random() * tags.length)]);
+  }
 
-    return extraMagnetWords;
-  };
+  return extraMagnetWords;
+};
 
-  // Constructs the random poem
-  const randomPoem = function() {
-    return [
-      randomElement(opening),
-      randomElement(verbs),
-      randomElement(objects),
-      randomElement(nouns),
-      randomElement(tags)
-    ];
-  };
+// Constructs the random poem
+const randomPoem = function() {
+  return [
+    randomElement(opening),
+    randomElement(verbs),
+    randomElement(objects),
+    randomElement(nouns),
+    randomElement(tags)
+  ];
+};
 
-  const calculatePlacement = function() {
-    let result = 0;
-    let neg = Math.floor(Math.random() * 2);
-    let magnitude = Math.floor(Math.random() * 100);
+const calculatePlacement = function() {
+  let result = 0;
+  let neg = Math.floor(Math.random() * 2);
+  let magnitude = Math.floor(Math.random() * 100);
 
-    if(neg === 0) {
-      result = -1 * magnitude;
-    } else {
-      result = magnitude;
-    }
+  if(neg === 0) {
+    result = -1 * magnitude;
+  } else {
+    result = magnitude;
+  }
 
-    return result;
-  };
+  return result;
+};
